c8d52f03b48918fbd2c113f649472b585b6e0ccc
daveyjones94 <g00227665@W213-16.academic.raritanval.edu>
2015-10-19 21:03:00 -0400

    Got a ball bouncing around!
    Now I'm just trying to figure out how to get them to not overlap the
    lines, but from here, I should pretty much be set to go!

--- BallDemo.java
+++ BallDemo.java
@@ -50,4 +50,41 @@ public class BallDemo
             }
         }
     }
+    
+    /**
+     * Simulate any number of balls bouncing around infinitely inside a box.
+     */
+    public void boxBounce()
+    {
+        int ground = 400;   // position of the ground line
+        int ceiling = 100;  // position of the ceiling line
+        int leftWall = 50;  // position of the left wall line
+        int rightWall = 550;    //position of the right wall line
+
+        myCanvas.setVisible(true);
+
+        // draw the ground
+        myCanvas.drawLine(50, ground, 550, ground);
+        myCanvas.drawLine(50, ceiling, 550, ceiling);
+        myCanvas.drawLine(leftWall, 100, leftWall, 400);
+        myCanvas.drawLine(rightWall, 100, rightWall, 400);
+
+        // crate and show the balls
+        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+        ball.draw();
+        BoxBall ball2 = new BoxBall(100, 100, 16, Color.RED, ground, leftWall, rightWall,
+                                    ceiling, myCanvas);
+
+        // make them bounce
+        boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
+            // stop once ball has travelled a certain distance on x axis
+            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+                finished = true;
+            }
+        }
+    }
 }
---
+++ BoxBall.java
@@ -0,0 +1,123 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+/**
+ * The BoxBall class creates balls to be drawn inside a canvas without speed degradation or slow
+ * down due to gravity. The BoxBalls should be able to bounce of any wall continuously and rebound
+ * with full speed, going into other walls.
+ * 
+ * @author David Martinez
+ * @version 1.0 (10-19-2015)
+ */
+public class BoxBall
+{
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private final int groundPosition;      // y position of ground
+    private final int leftWallPosition;
+    private final int rightWallPosition;
+    private final int ceilingPosition;
+    private Canvas canvas;
+    private int ySpeed = 2;                // initial downward speed
+    private int xSpeed = 6;
+
+    /**
+     * Constructor for objects of class BouncingBall
+     *
+     * @param xPos  the horizontal coordinate of the ball
+     * @param yPos  the vertical coordinate of the ball
+     * @param ballDiameter  the diameter (in pixels) of the ball
+     * @param ballColor  the color of the ball
+     * @param groundPos  the position of the ground (where the wall will bounce)
+     * @param lWallPos  the position of the left wall
+     * @param rWallPos  the position of the right wall
+     * @param ceilPos   the position of the ceiling
+     * @param drawingCanvas  the canvas to draw this ball on
+     */
+    public BoxBall(int xPos, int yPos, int ballDiameter, Color ballColor,
+                        int groundPos, int lWallPos, int rWallPos, int ceilPos,
+                        Canvas drawingCanvas)
+    {
+        xPosition = xPos;
+        yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
+        groundPosition = groundPos;
+        leftWallPosition = lWallPos;
+        rightWallPosition = rWallPos;
+        ceilingPosition = ceilPos;
+        canvas = drawingCanvas;
+    }
+
+    /**
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+            
+        // compute new position
+        yPosition += ySpeed;
+        xPosition += xSpeed;
+
+        // check if it has hit any of the walls
+        if(yPosition >= (groundPosition - diameter)) {
+            yPosition = (int)(groundPosition - diameter);
+            ySpeed = -ySpeed;
+        } 
+        else if (yPosition <= (ceilingPosition + diameter)) {
+            yPosition = (int)(ceilingPosition - diameter);
+            ySpeed = -ySpeed;
+        }
+        if (xPosition <= (leftWallPosition + diameter)) {
+            xPosition = (int)(leftWallPosition - diameter);
+            xSpeed = -xSpeed;
+        } 
+        else if (xPosition >= (rightWallPosition - diameter)) {
+            xPosition = (int)(rightWallPosition - diameter);
+            xSpeed = -xSpeed;
+        }
+        
+        // draw again at new position
+        draw();
+    }    
+
+    /**
+     * return the horizontal position of this ball
+     * @return  the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     * @return  the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    }
+}
